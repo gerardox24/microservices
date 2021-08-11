56563ee3ccf395d39a9197522007673419865f63
Johnny Ramos <jramos@baufest.com>
2021-08-11 11:37:05 -0500

    adding eureka registry config

---
+++ registry-service/src/test/java/com/baufest/store/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baufest.store.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
