6a03abf788be017e2cbf03b8a0c187744645d0d8
Johnny Ramos <jramos@baufest.com>
2021-08-12 18:01:40 -0500

    adding admin service and sleuth config

---
+++ admin-service/src/test/java/com/baufest/store/admin/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baufest.store.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
